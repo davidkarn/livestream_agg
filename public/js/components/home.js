86024594dd0e7ef941c697d9f810acde75dc55aa
David Karn <davidkarnseattle@gmail.com>
2017-03-26 12:22:37 -0700

    initialize actions and reducers

--- public/js/components/home.js
+++ public/js/components/home.js
@@ -2,6 +2,7 @@ import {load_streams} from 'actions'
 import Konva              from 'react-konva'
 import __                 from 'jsml'
 import InfernoRedux       from 'inferno-redux'
+import Home               from 'components/home'
 import config             from 'config'
 import Component          from 'inferno-component'
 
@@ -10,7 +11,7 @@ function map_state_to_props(state) {
 
 function map_dispatch_to_props(dispatch) {
     var act = 
-        {load_streamd:        (term, geoloc)  => dispatch(load_streams(term, geoloc))}
+        {load_streams:        (term, geoloc)  => dispatch(load_streams(term, geoloc))}
     return {act}}
 
 class Home extends Component {
@@ -19,12 +20,7 @@ class Home extends Component {
         var me = this }
     
     render() {
-        var viewing       = this.props.groups.viewing || false
-        var viewing_name  = viewing ? this.props.groups.viewing.page : 'private_galaxy'
-        var comp = this.get_component()
-
-        return __('div', {key: 'rootdiv',
-                          onScroll: this.on_scroll.bind(this)},
-                  comp)}}
+        return __(Home, {data: this.props.streams,
+                         act:  this.props.act})}}
 
 export default InfernoRedux.connect(map_state_to_props, map_dispatch_to_props)(Home)
