7fe9bfadb29b6edfb68dcb6747df4b224e3a5b7c
David Karn <davidkarnseattle@gmail.com>
2017-03-26 10:53:03 -0700

    search livestreams on periscope

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+var Entities    = require('html-entities').XmlEntities;
+var entities    = new Entities();
+var request     = require('request');
 var Twitter     = require('twitter')
 var twitter_key = 'o4PvV5Pj4po3DIZjab49AAsI8'
  
@@ -12,18 +15,24 @@ function search_twitter(term) {
     client.get('search/tweets', {q: term + ' filter:periscope'}, function(error, tweets, response) {
 	console.log('got tweets', {tweets})
 	for (var i in tweets) console.log(tweets[i])
-//	next(tweets)
+	next(tweets)
 
     });}
 
-function lookup_by_search_term() {
-    var request = require('request');
-    request('http://www.google.com', function (error, response, body) {
-	console.log('error:', error); // Print the error if one occurred
-	console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-	console.log('body:', body); // Print the HTML for the Google homepage.
+function values(x) {
+    var ar = []
+    for (var i in x) ar.push(x[i])
+    return ar }
+
+function search_periscope(term, next) {
+    request('https://www.periscope.tv/search?q=' + term, function (error, response, body) {
+        body = body.replace(/(.||[\r\n\t])*data-store="/im, '')
+        body = body.replace(/"><div id="PageView(.||[\r\n\t])*/im, '')
+        body = entities.decode(body)
+        body = JSON.parse(body)
+        next(values(body.BroadcastCache.broadcasts).map((x) => x.broadcast))
+//	console.log(JSON.stringify(body, undefined, 2)); // Print the HTML for the Google homepage.
     });}
-
-
-search_twitter("maga")
-search_twitter("trump")
+//search_twitter("maga")
+//search_twitter("trump")
+search_periscope("maga", (x) => console.log(x))
